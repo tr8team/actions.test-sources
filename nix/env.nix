2e494fd75be7e96e244d89ebed88d197856c8af2
Kirin nee <kirinnee97@gmail.com>
2023-03-04 20:54:48 +0800

    Initial commit

---
+++ nix/env.nix
@@ -0,0 +1,46 @@
+{ nixpkgs ? import <nixpkgs> { } }:
+let pkgs = import ./packages.nix { inherit nixpkgs; }; in
+with pkgs;
+{
+  system = [
+    coreutils
+    gnugrep
+    findutils
+    gnused
+    jq
+  ];
+
+  main = [
+    pls
+    nodejs-16_x
+    pnpm
+    gattai
+  ];
+
+  dev = [
+    pnpm
+    webstorm
+  ];
+
+  lint = [
+    action_docs
+    precommit-patch-nix
+    pre-commit
+    nixpkgs-fmt
+    prettier
+    shfmt
+    shellcheck
+    gitlint
+    sg
+  ];
+
+  ci = [
+    git
+    awscli2
+  ];
+
+  releaser = [
+    node18
+    sg
+  ];
+}
---
+++ nix/packages.nix
@@ -0,0 +1,44 @@
+{ nixpkgs ? import <nixpkgs> { } }:
+let
+  pkgs = rec {
+    atomi_classic = (
+      with import (fetchTarball "https://github.com/kirinnee/test-nix-repo/archive/refs/tags/v8.1.0.tar.gz");
+      {
+        inherit sg;
+      }
+    );
+    atomi = (
+      with import (fetchTarball "https://github.com/kirinnee/test-nix-repo/archive/refs/tags/v17.1.0.tar.gz");
+      {
+        inherit pls precommit-patch-nix gattai action_docs;
+      }
+    );
+    "Unstable 21st Feb 2023" = (
+      with import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/5ab8b5ae26e6a4b781bdebdfd131c054f0b96e70.tar.gz") { };
+      {
+        inherit
+          coreutils
+          gnugrep
+          awscli2
+          pre-commit
+          gitlint
+          nodejs-16_x
+          jq
+          nixpkgs-fmt
+          shfmt
+          findutils
+          gnused
+          git
+          shellcheck;
+        node18 = nodejs;
+        webstorm = jetbrains.webstorm;
+        prettier = nodePackages.prettier;
+        pnpm = nodePackages.pnpm;
+      }
+    );
+  };
+in
+with pkgs;
+atomi //
+atomi_classic //
+pkgs."Unstable 21st Feb 2023"
